df36b6c2d27203707e82ef5ad5858dea91ecc5b1
Arnold <arnoldjair@gmail.com>
2019-07-26 11:12:57 -0500

    More options

--- routes/options.js
+++ routes/options.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET options listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let body = {
     success: true,
     message: 'User options',
@@ -14,22 +14,26 @@ router.get('/', function(req, res, next) {
         icon: 'fa-user-alt',
         route: 'adm_users',
         options: [
-          {id: 1, code: 'adm_users_users', name: 'Usuarios', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios'}
+          { id: 10, code: 'adm_users_users', name: 'Usuarios', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios' },
+          { id: 11, code: 'adm_users_users', name: 'Perfiles', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios' },
+          { id: 12, code: 'adm_users_users', name: 'Reporte de usuarios', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios' },
+          { id: 13, code: 'adm_users_users', name: 'Reporte de perfiles', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios' },
+          { id: 14, code: 'adm_users_users', name: 'Reporte de auditoría', icon: 'fa-chevron-right', route: 'administrador-de-usuarios/usuarios' }
         ]
       },
       {
-        id: 5,
-	code: 'test',
-	name: 'Test',
-	icon: 'fa-trash-alt',
-	route: '#'
+        id: 2,
+        code: 'test',
+        name: 'Test',
+        icon: 'fa-trash-alt',
+        route: '#'
       }
     ]
   }
-  if(req.query.user === 'vpechera'){
+  if (req.query.user === 'vpechera') {
     res.jsonp(body);
   } else {
-    res.jsonp({success: false, message: 'el usuario no tiene opciones de menú', data: []});
+    res.jsonp({ success: false, message: 'el usuario no tiene opciones de menú', data: [] });
   }
 
 });
